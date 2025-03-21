d0fb39c516567dd82bf5ba48217cef5dd2acbeaa
Tran-Ngoc-Qui <tranngocqui.151020@gmail.com>
2025-03-21 18:14:52 +0700

    tao lai project

---
+++ src/main/java/edu/uth/skincarebookingsystem/SkincareBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.uth.skincarebookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkincareBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkincareBookingSystemApplication.class, args);
+	}
+
+}
